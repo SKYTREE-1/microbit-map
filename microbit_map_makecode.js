00993288de93b02a4a49838207751956c0756cd0
sky <sasakura.michiko@gmail.com>
2026-09-24 15:33:55 +0900

    Refactor button event handlers for micro:bit
    Removed duplicate Button.A event handler and added Button.B event handler.

--- microbit_map_makecode.js
+++ microbit_map_makecode.js
@@ -2,10 +2,6 @@
 // Webページから U / D / L / R / X を受け取り、LEDに矢印を表示します。
 // まずこのコードをMakeCodeに貼り付け、micro:bitへ転送してください。
 
-input.onButtonPressed(Button.A, function () {
-    basic.showIcon(IconNames.SmallDiamond)
-    bluetooth.uartWriteString("B\\n")
-})
 
 bluetooth.onUartDataReceived(serial.delimiters(Delimiters.NewLine), function () {
     cmd = bluetooth.uartReadUntil(serial.delimiters(Delimiters.NewLine))
@@ -24,10 +20,17 @@ bluetooth.onUartDataReceived(serial.delimiters(Delimiters.NewLine), function () 
         basic.clearScreen()
     }
 })
+
 input.onButtonPressed(Button.B, function () {
     basic.showIcon(IconNames.SmallDiamond)
     bluetooth.uartWriteString("B\\n")
 })
+
+input.onButtonPressed(Button.A, function () {
+    basic.showIcon(IconNames.SmallDiamond)
+    bluetooth.uartWriteString("B\\n")
+})
+
 let cmd = ""
 bluetooth.startUartService()
 basic.showString("MAP")
